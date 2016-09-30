1c9d85a9e985fff6ea419ee2148639c8df34907a
liuqianshuai <158642510@qq.com>
2016-09-30 19:41:04 +0800

    add static method

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
 public class Hello {
 	public static void main(String args[]) {
-		System.out.println("Hello git!");
-		System.out.println("Hello branch!");
-		System.out.println("Hello jack!");
+		System.out.println("Happy New Year") ;
+		System.out.println("Happy MySelf") ;
+		print() ;
 	}
+	public static void print() {
+	   System.out.println("*********************") ;
+	   System.out.println("*   Hello World !   *") ;
+	   System.out.println("*********************") ;
+	 }
 }
+
