ed4fc61c62dd26305ac931e1be47cc593b38502f
liuqianshuai <158642510@qq.com>
2016-10-01 16:05:18 +0800

    brh commit

---
+++ Emp.java
@@ -0,0 +1,9 @@
+public class Hello {
+	public static final String COMPANY_NAME = "yootk" ;
+	public static void main(String args[]) {
+		System.out.println("Happy New Year") ;
+		System.out.println("Happy MySelf") ;
+	}
+}
+
+
