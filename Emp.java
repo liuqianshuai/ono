7e3c5c98e0777e83aa9f689f0df11036e9d835d5
liuqianshuai <158642510@qq.com>
2016-10-01 16:41:34 +0800

    Chage Emp.java

--- Emp.java
+++ Emp.java
@@ -1,10 +1,15 @@
 import java.io.* ;
 import java.util.* ;
 public class Emp implements Serializable {
+<<<<<<< Updated upstream
 
 	private Integer empno;
 	private String ename;
 	private String oo;
+=======
+	private Integer empno ;
+	private String ename ;
+>>>>>>> Stashed changes
 	private Date hiredate ;
 }
 
