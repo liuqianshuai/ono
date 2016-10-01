f441e7a75e0b1db0dcfa7f75d6c8ce18ef5e17f0
liuqianshuai <158642510@qq.com>
2016-10-01 16:44:15 +0800

    change Emp.java

--- Emp.java
+++ Emp.java
@@ -1,6 +1,10 @@
+import java.io.* ;
+import java.util.* ;
 public class Emp implements Serializable {
-	private Integer empno;
-	private String ename;
+	private Integer empno ;
+	private String ename ;
+	private Date hiredate ;
 }
+
 
 
